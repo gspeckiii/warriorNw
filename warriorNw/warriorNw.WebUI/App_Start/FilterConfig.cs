2554c3d942ddfb571f51b87e5e15c6efa980dfa4
George <g.peck.iii@gmail.com>
2018-10-24 11:12:05 -0700

    Intial solution setup

---
+++ warriorNw/warriorNw.WebUI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace warriorNw.WebUI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ warriorNw/warriorNw.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(warriorNw.WebUI.Startup))]
+namespace warriorNw.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
